832c37c54f0cf5617441ab7fd4d7a3d5cb666628
Akash Prajapati <akashprajapati1849@gmail.com>
2024-02-14 19:26:55 +0530

    217. Contains Duplicate

---
+++ LeetCode/solution/ContainsDuplicate.java
@@ -0,0 +1,28 @@
+import java.util.HashSet;
+
+/**
+ * https://leetcode.com/problems/contains-duplicate/description/
+ * @author Akash Prajapati
+ */
+public class Main {
+    public static void main(String[] args) {
+//        int[] nums = new int[]{1, 2, 1, 4};
+        int[] nums = new int[]{1,1,1,3,3,4,3,2,4,2};
+//        int[] nums = new int[]{1,2,3,4};
+        Solution obj = new Solution();
+        System.out.println(obj.containsDuplicate(nums));
+    }
+}
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> container = new HashSet<>();
+        for (int num : nums) {
+            if (container.contains(num)) {
+                return true;
+            }
+            container.add(num);
+        }
+        return false;
+    }
+}
