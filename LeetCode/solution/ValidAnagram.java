282385b445c8a4f4433928e7550c961a503040ec
Akash Prajapati <akashprajapati1849@gmail.com>
2024-02-19 12:01:02 +0530

    242. Valid Anagram added another solutions

--- LeetCode/solution/ValidAnagram.java
+++ LeetCode/solution/ValidAnagram.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Scanner;
 
 /**
@@ -47,6 +48,7 @@ public class ValidAnagram {
 /**
  * optimised easy solution
  */
+/*
 class Solution {
     public boolean isAnagram(String s, String t) {
         char[] sChars = s.toCharArray();
@@ -56,5 +58,62 @@ class Solution {
         Arrays.sort(tChars);
 
         return Arrays.equals(sChars, tChars);
+    }
+}*/
+
+/**
+ * Approach 2: Hash Table (Using HashMap)
+ */
+/*
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character, Integer> count = new HashMap<>();
+
+        // Count the frequency of characters in string s
+        for (char x : s.toCharArray()) {
+            count.put(x, count.getOrDefault(x, 0) + 1);
+        }
+
+        // Decrement the frequency of characters in string t
+        for (char x : t.toCharArray()) {
+            count.put(x, count.getOrDefault(x, 0) - 1);
+        }
+
+        // Check if any character has non-zero frequency
+        for (int val : count.values()) {
+            if (val != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}*/
+
+/**
+ * Approach 3: Hash Table (Using Array)
+ */
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] count = new int[26];
+
+        // Count the frequency of characters in string s
+        for (char x : s.toCharArray()) {
+            count[x - 'a']++;
+        }
+
+        // Decrement the frequency of characters in string t
+        for (char x : t.toCharArray()) {
+            count[x - 'a']--;
+        }
+
+        // Check if any character has non-zero frequency
+        for (int val : count) {
+            if (val != 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
